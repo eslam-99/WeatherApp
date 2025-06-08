7c553963ec2e42fb57150ac12ce45ef694158aaf
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-08 12:06:48 +0300

    add app assets

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkBackgroundGradiant = listOf(
+    Color(0xFF060414),
+    Color(0xFF0D0C19),
+)
+
+val LightBackgroundGradiant = listOf(
+    Color(0xFF87CEFA),
+    Color(0xFFFFFFFF),
+)
+
+val LightTextColor = Color(0xFF000000)
+val LightTextColor87 = Color(0x57000000)
+val LightTextColor60 = Color(0x3C000000)
+val DarkTextColor = Color(0xFFFFFFFF)
+val DarkTextColor87 = Color(0x57FFFFFF)
+val DarkTextColor60 = Color(0x3CFFFFFF)
---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Fonts.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.weatherapp.R
+
+val Urbanist = FontFamily(
+    Font(R.font.urbanist)
+)
