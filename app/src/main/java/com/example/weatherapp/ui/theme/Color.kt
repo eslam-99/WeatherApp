dee13d614d63afe0f0d836cb9e535e514f2c248c
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-09 22:51:18 +0300

    update drawables

--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -10,19 +10,28 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val DarkBackgroundColor = Color(0xFF060414)
+val DarkBackgroundColor70 = Color(0xFF060414).copy(alpha = 0.7f)
+val LightBackgroundColor = Color(0xFFFFFFFF)
+val LightBackgroundColor70 = Color(0xFFFFFFFF).copy(alpha = 0.7f)
+val DarkElementBackgroundColor = Color(0xFFFFFFFF)
+val DarkElementBackgroundColor70 = Color(0xFFFFFFFF).copy(alpha = 0.7f)
+val LightElementBackgroundColor = Color(0xFF060414)
+val LightElementBackgroundColor70 = Color(0xFF060414).copy(alpha = 0.7f)
 val DarkBackgroundGradiant = listOf(
     Color(0xFF060414),
     Color(0xFF0D0C19),
 )
-
 val LightBackgroundGradiant = listOf(
     Color(0xFF87CEFA),
     Color(0xFFFFFFFF),
 )
+val DarkBorderColor = Color(0xFFFFFFFF).copy(alpha = 0.08f)
+val LightBorderColor = Color(0xFF060414).copy(alpha = 0.08f)
 
 val LightTextColor = Color(0xFF000000)
-val LightTextColor87 = Color(0x57000000)
-val LightTextColor60 = Color(0x3C000000)
+val LightTextColor87 = Color(0xFF000000).copy(alpha = 0.87f)
+val LightTextColor60 = Color(0xFF000000).copy(alpha = 0.6f)
 val DarkTextColor = Color(0xFFFFFFFF)
-val DarkTextColor87 = Color(0x57FFFFFF)
-val DarkTextColor60 = Color(0x3CFFFFFF)
+val DarkTextColor87 = Color(0xFFFFFFFF).copy(alpha = 0.87f)
+val DarkTextColor60 = Color(0xFFFFFFFF).copy(alpha = 0.6f)
